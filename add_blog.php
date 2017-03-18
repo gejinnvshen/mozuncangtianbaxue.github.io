a18593516b67c5ad260d715df1878fb052df180a
mozuncangtianbaxue <15034173008@163.com>
2017-03-18 11:07:55 +0800

    add index.html

---
+++ add_blog.php
@@ -0,0 +1,27 @@
+
+<?php
+	require 'config.php';
+	
+	$query="INSERT INTO blog_two(title,content,date)
+						VALUES ('{$_POST['title']}','{$_POST['content']}',NOW())";
+
+	mysql_query($query) or die('新增失败!'.mysql_error());
+
+	echo mysql_affected_rows();
+
+	mysql_close();
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- js/index.js
+++ js/index.js
@@ -37,18 +37,90 @@ $(document).ready(function(){
 	$('#personal .member').hover(function(){
 		//alert('de')
 		$('#personal .person').show();
+		$('#personal .person li').eq(0).click(function(){
+			$('#blog').show();
+		})
 	},function(){
 		$('#personal .person').hide();
 	})
 
-	//拖拽
-
-	$('#blog .submit').click(function(){
-	var a=$('#blog form dd .title').val();
-		$('#main .content p').html(a);	
-		alert(a);
+	$('#blog h2 img').click(function(){
+		$('#blog').hide();
 	})
-	$.ajax(alert('dd'));
+	//注册
+	$('#personal .reg').click(function(){
+		$('#zhuce').show();
+	})
+	$('#zhuce h2 img').click(function(){
+		$('#zhuce').hide();
+	})
+
+	//登录
+	$('#personal .login').click(function(){
+		$('#login').show();
+	})
+	$('#login h2 img').click(function(){
+		$('#login').hide();
+	})
+
+	
+
+	//发表博文
+	$('#')
+	$('#blog .submit').click(function(){
+	
+		$.ajax({
+			type:'post',
+			url:'add_blog.php',
+			data:$('form').serialize(),
+			success:function(data){
+				$.ajax({
+					type:'post',
+					url:'get_blog.php',
+					data:{},
+					success:function(data){
+						var json = JSON.parse(data);
+						//alert(json[0].content);
+						for(var i=0;i<json.length;i++){
+							$('#main .content h3').eq(i).html(function(){
+								return json[i].title;
+							});
+							$('#main .content p').eq(i).html(function(){
+								return json[i].content;
+							});				
+						}
+					},
+					async:true
+				});
+			},
+			async:true
+		});
+		
+		
+
+	})
+	//获取博文.这个ajax要写在click方法外，否则刷新一下页面的内容消失
+	$.ajax({
+		type:'post',
+		url:'get_blog.php',
+		data:{},
+		success:function(data){
+			var json = JSON.parse(data);
+			//alert(json[0].title);
+			for(var i=0;i<json.length;i++){
+				$('#main .content h3').eq(i).html(function(){
+					return json[i].title;
+				});
+				$('#main .content p').eq(i).html(function(){
+					return json[i].content;
+				});
+			}
+			
+		},
+		async:true
+	});
+
+
 })
 
 
